e7b4d139f07da193f6662ca681701674a00a8c42
Neha Sanserwal <neha.sanserwal@thoughtworks.com>
2020-04-30 17:28:55 +0530

    created function add_to_end.

--- list.c
+++ list.c
@@ -5,5 +5,25 @@ List_ptr create_list(void)
   List_ptr list = malloc(sizeof(List));
   list->head = NULL;
   list->last = NULL;
+  list->count = 0;
   return list;
+}
+
+Status add_to_end(List_ptr list, int value)
+{
+  Node_ptr node = malloc(sizeof(Node));
+  node->value = value;
+  node->next = NULL;
+  if (!list->head)
+  {
+    list->head = node;
+    list->last = node;
+  }
+  else
+  {
+    list->last->next = node;
+    list->last = node;
+  }
+  list->count = ++list->count;
+  return Success;
 }
--- numbers.c
+++ numbers.c
@@ -1,14 +1,29 @@
 #include <stdio.h>
 #include "list.h"
+
 void printOptions()
 {
   printf("(a) add a number to the end of the list\n(b) add a number to the start of the list\n");
 }
+
 int main(void)
 {
-  List_ptr list = create_list();
   char option;
+  int value;
+  List_ptr list = create_list();
   printOptions();
   scanf("%c", &option);
+
+  switch (option)
+  {
+  case 'a':
+    printf("Enter a value:");
+    scanf("%d", &value);
+    Status status = add_to_end(list, value);
+    break;
+
+  default:
+    break;
+  }
   return 0;
 }
